4be37c2ac239b597f1a54fb83a666d88e19c5b73
Myeongjin <aranet100@gmail.com>
2013-12-18 14:22:43 +0900

    Add Korean translation

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['token'] = '대상 activecollab API에 대한 비밀 토큰';
+$lang['url'] = '대상 activecollab 인스턴스의 API 베이스 URL';
